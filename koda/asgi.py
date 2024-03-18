fab79c2c1f5326fd9295bfacae80a7e594fce953
Ugochukwu Nwachukwu <danielnwachukwu5738@gmail.com>
2024-03-19 00:15:25 +0100

    Update: reviewed asgi.py

--- koda/asgi.py
+++ koda/asgi.py
@@ -1,22 +1,26 @@
 import os
 
-# import django
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from decouple import config
 from django.core.asgi import get_asgi_application
 
-import assistant.routing
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", config("DJANGO_SETTINGS_MODULE"))
-# django.setup()
 
+# Setup Django before loading the application.
+django.setup()
+
+# Import the routing after Django has been set up.
+import assistant.routing
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(
-            URLRouter(assistant.routing.websocket_urlpatterns)
+        "http": get_asgi_application(),  # Define the ASGI application to use for HTTP protocols.
+        "websocket": AuthMiddlewareStack(  # Define the ASGI application to use for WebSocket protocols.
+            URLRouter(
+                assistant.routing.websocket_urlpatterns  # Use the WebSocket URL routing defined in assistant.routing.
+            )
         ),
     }
 )
